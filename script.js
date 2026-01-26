20463a87321980febc00487003fcd3ce85b0bf52
OKSnappy <80738796+Snapix@users.noreply.github.com>
2026-01-26 06:25:54 +0000

    init: static TouchlessTouch website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  alert("Download coming soon 👀");
+  // later you can link:
+  // window.location.href = "https://github.com/Snapix/touchless/releases";
+});
